b99fd53e34f0a09acda1bb841153857b91811756
natively.dev <api@natively.dev>
2025-09-06 03:33:07 +0000

    feat: Center RCAST.NET player and set black background in playlist

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function PlaylistScreen() {
   const htmlContent = `
@@ -14,41 +14,63 @@ export default function PlaylistScreen() {
           margin: 0;
           padding: 0;
           height: 100vh;
+          background-color: black;
+          display: flex;
+          justify-content: center;
+          align-items: center;
           overflow: hidden;
+        }
+        .container {
+          width: 90%;
+          max-width: 800px;
+          height: 80%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
         }
         iframe {
           width: 100%;
-          height: 100vh;
+          height: 100%;
           border: none;
+          border-radius: 8px;
         }
       </style>
     </head>
     <body>
-      <!-- RCAST.NET - START EMBEDDED PLAYER --> 
-      <iframe width="100%" height="560" src="https://players.rcast.net/playlisthistory/69431" frameborder="0" scrolling="no" allow="autoplay"></iframe> 
-      <div style="overflow:hidden; height:0px; width:0px;"><a href="https://www.rcast.net" title="Internet Radio Hosting">RCAST.NET</a></div>
-      <!-- RCAST.NET - END EMBEDDED PLAYER -->
+      <div class="container">
+        <!-- RCAST.NET - START EMBEDDED PLAYER --> 
+        <iframe width="100%" height="560" src="https://players.rcast.net/playlisthistory/69431" frameborder="0" scrolling="no" allow="autoplay"></iframe> 
+        <div style="overflow:hidden; height:0px; width:0px;"><a href="https://www.rcast.net" title="Internet Radio Hosting">RCAST.NET</a></div>
+        <!-- RCAST.NET - END EMBEDDED PLAYER -->
+      </div>
     </body>
     </html>
   `;
 
-  console.log('Playlist screen rendered with embedded RCAST.NET player');
+  console.log('Playlist screen rendered with centered embedded RCAST.NET player on black background');
 
   return (
-    <WebView
-      style={styles.webview}
-      originWhitelist={['*']}
-      source={{ html: htmlContent }}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      allowsInlineMediaPlayback={true}
-      mediaPlaybackRequiresUserAction={false}
-    />
+    <View style={styles.container}>
+      <WebView
+        style={styles.webview}
+        originWhitelist={['*']}
+        source={{ html: htmlContent }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUserAction={false}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
   webview: {
     flex: 1,
+    backgroundColor: 'black',
   },
 });
